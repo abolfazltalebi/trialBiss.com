f09f8137a00e7e83daab96867a8c9e3e4dd95753
Your Name <you@example.com>
2025-02-11 12:42:55 +0330

    fixed bug

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -3,7 +3,7 @@ import notfound from "@/assets/images/404.webp"
 
 export default function NotFound() {
     return(
-        <div className="flex items-center flex-col">
+        <div className="flex items-center flex-col mb-6">
             <img src={notfound} alt="" className="px-12 md:px-0 md:w-1/2" />
             <Button variant="notFountButton">back to home</Button>
         </div>
